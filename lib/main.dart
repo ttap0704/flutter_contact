e68c41ad5146ca45aa34070442cca9e02c2b1425
LeeDaeGyu <ttap0704@naver.com>
2022-10-27 08:31:32 +0900

    practice props function

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
+  var total = 3;
   var name = ['김영숙', '홍길동', '피자집'];
   var likes = [0, 0, 0];
+
+  void test(String new_name) {
+    print(name);
+    setState(() {
+      name.add(new_name);
+      likes.add(0);
+      total++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,24 +37,13 @@ class _MyAppState extends State<MyApp> {
           showDialog(
             context: context,
             builder: (context) {
-              return AlertDialog(
-                title: Text('Contact'),
-                content: TextField(),
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Cancel')),
-                  TextButton(onPressed: () {}, child: Text('Ok')),
-                ],
-              );
+              return CustomTest(onSubmit: test);
             },
           );
         },
       ),
       appBar: AppBar(
-        title: Text('ㅗㅑㅗㅑ'),
+        title: Text(total.toString()),
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
@@ -54,8 +52,38 @@ class _MyAppState extends State<MyApp> {
             title: Text(name[index]),
           );
         },
-        itemCount: 3,
+        itemCount: total,
       ),
+    );
+  }
+}
+
+class CustomTest extends StatelessWidget {
+  CustomTest({Key? key, this.onSubmit}) : super(key: key);
+  final onSubmit;
+
+  var inputData = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Contact'),
+      content: TextField(
+        controller: inputData,
+      ),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel')),
+        TextButton(
+            onPressed: () {
+              onSubmit(inputData.text);
+              Navigator.pop(context);
+            },
+            child: Text('OK')),
+      ],
     );
   }
 }
