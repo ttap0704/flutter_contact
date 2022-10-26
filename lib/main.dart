e69f6d903e84388b96d40d2e83c60038ae89fa3a
LeeDaeGyu <ttap0704@naver.com>
2022-10-26 09:22:50 +0900

    practice state

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = ['김영숙', '홍길동', '피자집'];
+  var likes = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(), body: ShopItem()));
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('ㅗㅑㅗㅑ'),
+            ),
+            bottomNavigationBar: BottomNavBar(),
+            body: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Text(likes[index].toString()),
+                  title: Text(name[index]),
+                  trailing: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          likes[index]++;
+                        });
+                      },
+                      child: Text('좋아요')),
+                );
+              },
+              itemCount: 3,
+            )));
   }
 }
 
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Text('안'),
+    return BottomAppBar(
+      child: Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [Icon(Icons.call), Icon(Icons.note), Icon(Icons.file_copy)],
+        ),
+      ),
     );
   }
 }
